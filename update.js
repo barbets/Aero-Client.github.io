30229d1a37bb8faf05fd284333e13e8076926249
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-06-29 07:39:36 -0700

    Update update.js

--- update.js
+++ update.js
@@ -1,1 +1,9 @@
-return
+var xhttp9 = new XMLHttpRequest();
+xhttp9.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 404) {
+        var image = document.getElementById('launchButton');
+        image.src = "images/launch/servererror.png";
+    }
+};
+xhttp9.open("GET", "https://github.com/Aero-Client/Aero-Client.github.io/blob/master/remotebackend.js", true);
+xhttp9.send();
